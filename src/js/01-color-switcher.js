cfdf99cfad06b36bee0a965449ba30ceeb344b75
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-10-31 19:24:51 +0200

    Add 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.stopBtn.disabled = true;
+refs.startBtn.addEventListener('click', startNewBgColorBody);
+refs.stopBtn.addEventListener('click', stopNewBgColorBody);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startNewBgColorBody() {
+  currentColor = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function stopNewBgColorBody() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(currentColor);
+}
