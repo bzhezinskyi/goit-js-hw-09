c6500b3d0e3035dc30f0dcb710d3ae2f0cb6f340
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-10-31 17:25:26 +0000

    Deploying to gh-pages from @ bzhezinskyi/goit-js-hw-09@cfdf99cfad06b36bee0a965449ba30ceeb344b75 🚀

---
+++ 01-color-switcher.a6c3cfe1.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){currentColor=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(currentColor)}));
+//# sourceMappingURL=01-color-switcher.a6c3cfe1.js.map
