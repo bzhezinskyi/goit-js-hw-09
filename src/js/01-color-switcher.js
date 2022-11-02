bdc409132c95fda536dd1467191dee5204ba2b11
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-11-02 12:15:44 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,22 +8,28 @@ refs.stopBtn.disabled = true;
 console.log('refs.stopBtn.disabled', refs.stopBtn.disabled);
 console.log('refs.startBtn.disabled', refs.startBtn.disabled);
 
-refs.startBtn.addEventListener('click', startNewBgColorBody);
+refs.startBtn.addEventListener('click', () => {
+  currentColor = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+});
 refs.stopBtn.addEventListener('click', stopNewBgColorBody);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function startNewBgColorBody() {
-  currentColor = setInterval(() => {
-    refs.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-  refs.startBtn.disabled = true;
-  refs.stopBtn.disabled = false;
-  console.log('refs.stopBtn.disabled', refs.stopBtn.disabled);
-  console.log('refs.startBtn.disabled', refs.startBtn.disabled);
-}
+// function startNewBgColorBody() {
+//   currentColor = setInterval(() => {
+//     refs.body.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+//   refs.startBtn.disabled = true;
+//   refs.stopBtn.disabled = false;
+//   console.log('refs.stopBtn.disabled', refs.stopBtn.disabled);
+//   console.log('refs.startBtn.disabled', refs.startBtn.disabled);
+// }
 
 function stopNewBgColorBody() {
   refs.startBtn.disabled = false;
