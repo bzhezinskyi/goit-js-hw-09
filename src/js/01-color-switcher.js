1c51cf8f67def101acec50bb28fd93b542997959
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-11-02 12:49:25 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,24 +8,22 @@ refs.stopBtn.disabled = true;
 refs.startBtn.addEventListener('click', startNewBgColorBody);
 refs.stopBtn.addEventListener('click', stopNewBgColorBody);
 
-function currentColor() {
-  color = setInterval(() => {
-    refs.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-}
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+let currentColor;
+
 function startNewBgColorBody() {
+  currentColor = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
   refs.startBtn.disabled = true;
   refs.stopBtn.disabled = false;
-  currentColor();
 }
 
 function stopNewBgColorBody() {
   refs.startBtn.disabled = false;
   refs.stopBtn.disabled = true;
-  clearInterval(color);
+  clearInterval(currentColor);
 }
